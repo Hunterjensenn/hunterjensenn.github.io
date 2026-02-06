8820c51d5230b0aa49751ffca4d3c94d090a03a0
Hunterjensenn <jensenhunter13@gmail.com>
2026-02-06 20:35:33 +0000

    Deploying to gh-pages from @ Hunterjensenn/hunterjensenn.github.io@396033244b8ca0e80cff80eac1c980319a637b9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,13 +417,13 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-pwm-signal-generation-and-monitoring-system",
+          title: 'PWM Signal Generation and Monitoring System',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-software-defined-radio-project",
+          title: 'Software Defined Radio Project',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
